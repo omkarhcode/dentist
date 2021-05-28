a91a4c7713d3a8386b251373de0d1448bcb28aef
omkarhcode <omkarh.code@gmail.com>
2021-05-28 18:46:10 +0530

    Added more pages

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
 	path('', views.home, name="home"),
+	path('about', views.about, name="about"),
+	path('patients', views.patients, name="patients"),
+	path('news', views.news, name="news"),
+	path('services', views.services, name="services"),
 	path('contact', views.contact, name="contact"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+
 
 # Create your views here.
 def home(request):
 	return render(request, 'home.html', {})
 
+def about(request):
+	return render(request, 'about.html', {})
+
+def patients(request):
+	return render(request, 'patients.html', {})
+
+def news(request):
+	return render(request, 'news.html', {})
+
+def services(request):
+	return render(request, 'services.html', {})
+
 def contact(request):
-	return render(request, 'contact.html', {})
+	if request.method == "POST":
+		user_name = request.POST['fullname']
+		user_email = request.POST['email']
+		user_message = request.POST['message']
+
+		# Send an email
+		send_mail('Email from '+user_name,user_message, user_email, ['omkarh.code@gmail.com'])
+
+		return render(request, 'contact.html', {'user_name': user_name})
+	else:
+		return render(request, 'contact.html', {})
+
